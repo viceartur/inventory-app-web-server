bfe947bcb3f34635acb322df735e69ab11029813
Artur Sopov <capone56j@gmail.com>
2025-08-06 12:17:12 -0400

    Added logs to ws

--- services/websocket/endpoints.go
+++ services/websocket/endpoints.go
@@ -9,15 +9,20 @@ import (
 
 // Web Socket Endpoint without any Parameters
 func WsEndpoint(w http.ResponseWriter, r *http.Request) {
+	log.Println("WebSocket connection attempt from:", r.RemoteAddr)
+
 	upgrader.CheckOrigin = func(r *http.Request) bool { return true }
+
 	ws, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Println("Upgrade error:", err)
+		log.Println("WebSocket upgrade error:", err)
 		return
 	}
 
 	vars := mux.Vars(r)
 	userRole := UserRole(vars["userRole"])
+
+	log.Printf("New WebSocket client connected. Role: %s, IP: %s\n", userRole, r.RemoteAddr)
 
 	addClient(ws, userRole)
 	go reader(ws)
